0ff47027e319b28d54f7892cb4c362003fad1821
Jeff Ogata <jeffogata@outlook.com>
2015-10-23 18:41:19 -0500

    initial implementation of code to scan and register mediatr handlers.

--- src/ContosoUniversity/Infrastructure/MediatRExtensions.cs
+++ src/ContosoUniversity/Infrastructure/MediatRExtensions.cs
@@ -1,20 +1,37 @@
 ﻿namespace ContosoUniversity.Infrastructure
 {
-    using System.Collections.Generic;
-    using ContosoUniversity.Features.Course;
+    using System.Linq;
+    using System.Reflection;
     using MediatR;
+    using Microsoft.Dnx.Runtime;
     using Microsoft.Framework.DependencyInjection;
 
     public static class MediatRExtensions
     {
         public static IServiceCollection AddMediatR(this IServiceCollection services)
         {
-            services.AddScoped<IMediator>(servicesProvider => new Mediator(servicesProvider.GetService, servicesProvider.GetServices));
+            services.AddScoped<IMediator>(
+                servicesProvider => new Mediator(servicesProvider.GetService, servicesProvider.GetServices));
 
-            services.AddScoped(typeof (IAsyncRequestHandler<Index.Query, Index.Result>), typeof (Index.Handler));
-            services.AddScoped(
-                typeof (IAsyncRequestHandler<Features.Department.Index.Query, List<Features.Department.Index.Model>>),
-                typeof (Features.Department.Index.QueryHandler));
+            var libraryManager = services.BuildServiceProvider().GetService<ILibraryManager>();
+
+            var assemblies = libraryManager
+                .GetReferencingLibraries("MediatR")
+                .Distinct()
+                .SelectMany(l => l.Assemblies)
+                .Select(Assembly.Load);
+
+            var handlerTypes = assemblies
+                .SelectMany(a => a.DefinedTypes)
+                .Where(typeInfo => typeInfo.GetInterfaces().Any(x =>
+                    x.IsGenericType && x.GetGenericTypeDefinition() == typeof (IAsyncRequestHandler<,>)));
+
+            foreach (var type in handlerTypes)
+            {
+                var interfaceType =
+                    type.GetInterfaces().First(x => x.GetGenericTypeDefinition() == typeof (IAsyncRequestHandler<,>));
+                services.AddScoped(interfaceType, type);
+            }
 
             return services;
         }
