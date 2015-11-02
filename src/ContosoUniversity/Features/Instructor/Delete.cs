1f53b8d15bc881bb4e770dd300d40817a5515538
Jeff Ogata <jeffogata@outlook.com>
2015-11-01 21:04:48 -0600

    add api controller for instructors.  add a todo note to revisit the use of cascading FK actions on Instructor deletion.

--- src/ContosoUniversity/Features/Instructor/Delete.cs
+++ src/ContosoUniversity/Features/Instructor/Delete.cs
@@ -12,6 +12,11 @@
 
         public class CommandHandler : DeleteCommandHandler<Instructor, Command>
         {
+            // todo: revisit use of FK actions when deleting instructors
+            // i updated the FKS to set dept admin to null and cascade delete of office assignment. the only practical 
+            // reason i see to do this outside of the database is to support a RDBMS that doesn't support cascading 
+            // FK actions, but even SQLite supports SET NULL and CASCADE: https://www.sqlite.org/foreignkeys.html#fk_actions
+
             public CommandHandler(ContosoUniversityContext dbContext) : base(dbContext)
             {
             }
