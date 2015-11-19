623b9362c0f6187bf131b876394ba29e73b2e71f
Jeff Ogata <jeffogata@outlook.com>
2015-11-19 02:05:46 -0600

    work around problems with path environment variable not being refreshed in the same session that dnx is installed.

--- run.cmd
+++ run.cmd
@@ -1,6 +1,10 @@
 @echo off
 cd %~dp0/src/ContosoUniversity
-start "" dnx web
+
+rem : starting dnx using PS because when run in the same session as dnx was installed, the Path will not be refreshed to include the
+rem : path to dnx.  i can set the path in the powershell command, but that is only visible within the command
+
+powershell -command "set-item -path env:Path -value $([System.Environment]::GetEnvironmentVariable('Path','Machine')+ ';' + [System.Environment]::GetEnvironmentVariable('Path','User')); start-process dnx web"
 timeout 5
 start "" http://localhost:5000
 
