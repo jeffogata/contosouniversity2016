2bae58109e9916cf7b7473e28da7f4dcb82614db
Jeff Ogata <jeffogata@outlook.com>
2015-12-19 10:37:15 -0600

    update rh.exe to latest from build server (removes the dependency on .NET 3.5, which the old rh.exe targeted).  removed unused tools (fixie, psake).  update makefile.shade, run.cmd and global.json to use latest asp.net 5 release (1.0.0-rc1-update1).

--- run.cmd
+++ run.cmd
@@ -4,7 +4,7 @@ cd %~dp0/src/ContosoUniversity
 rem : starting dnx using PS because when run in the same session as dnx was installed, the Path will not be refreshed to include the
 rem : path to dnx.  i can set the path in the powershell command, but that is only visible within the command
 
-powershell -command "set-item -path env:Path -value $([System.Environment]::GetEnvironmentVariable('Path','Machine')+ ';' + [System.Environment]::GetEnvironmentVariable('Path','User')); start-process dnx web"
+powershell -command "set-item -path env:Path -value $([System.Environment]::GetEnvironmentVariable('Path','Machine')+ ';' + [System.Environment]::GetEnvironmentVariable('Path','User')); dnvm use 1.0.0-rc1-update1 -r clr -a x86; start-process dnx web"
 timeout 5
 start "" http://localhost:5000
 
